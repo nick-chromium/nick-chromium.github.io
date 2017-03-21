92de93a6f6fc216a84f4b4e7fbf944f6eee9925f
nick-chromium <nick@chromium.org>
2017-03-21 11:02:49 -0700

    Update neterror-subframe.js

--- neterror-subframe.js
+++ neterror-subframe.js
@@ -1,7 +1,8 @@
 // JS file for csp-neterror-subframe.js
 
-document.getElementsById('meta')
 document.getElementById('meta-tags').innerText = document.head.innerHTML;
 
 
 var i = document.createElement('iframe');
+i.src = 'https://invalid.hostname.example.com/';
+document.body.append(i);
