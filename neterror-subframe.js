86c63c3fe6f783d281775d1a9806451c6e6376f9
nick-chromium <nick@chromium.org>
2017-03-21 11:00:57 -0700

    Create neterror-subframe.js

---
+++ neterror-subframe.js
@@ -0,0 +1,7 @@
+// JS file for csp-neterror-subframe.js
+
+document.getElementsById('meta')
+document.getElementById('meta-tags').innerText = document.head.innerHTML;
+
+
+var i = document.createElement('iframe');
