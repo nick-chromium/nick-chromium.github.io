278a83252d6c46d3326a6b26c569c9dcfab3a0a5
nick-chromium <nick@chromium.org>
2017-03-21 11:22:00 -0700

    Update neterror-subframe.js

--- neterror-subframe.js
+++ neterror-subframe.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", (e) => {
     document.getElementById('meta-tags').innerText = document.head.innerHTML;
     var i = document.createElement('iframe');
+    i.sandbox = '';
     i.src = 'https://invalid.hostname.example.com/';
     document.body.append(i);
 });
